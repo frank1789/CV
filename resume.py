7f00ec2f592a56023d48dc7293975d0756864ec8
Francesco <francesco.argentieri89@gmail.com>
2020-08-15 09:44:34 +0200

    add: refactor main file

--- resume.py
+++ resume.py
@@ -2,61 +2,20 @@
 # -*- coding: utf-8 -*-
 
 
-import errno
-import json
-import os
+import argparse
 import sys
 
-sys.path.insert(0, 'curriculum')
 from curriculum.factory import setup_curriculum_vitae
+from curriculum.identifying import Identifying
 
-
-class Registry:
-    def __init__(self) -> None:
-        self.__data = {}
-        pass
-
-    def read_from_file(self, namefile):
-        if os.path.exists(namefile):
-            with open(namefile) as infile:
-                self.__data = json.load(infile)
-        else:
-            raise FileNotFoundError(
-                errno.ENOENT, os.strerror(errno.ENOENT), namefile)
-
-    @property
-    def information(self) -> dict:
-        return self.__data
-
-    @information.setter
-    def information(self, a):
-        self.__data = a
-
-    @property
-    def personal_data(self) -> dict:
-        return self.__data["data"]
-
-    @property
-    def education(self) -> list:
-        return self.__data["data"]["education"]
-
-    @property
-    def experience(self) -> list:
-        return self.__data["experience"]
-
-    @property
-    def skills(self) -> list:
-        return self.__data["skillset"]
-
-    @property
-    def certificates(self) -> list:
-        return self.__data["certificate"]
-
+parser = argparse.ArgumentParser(description="Build fast your CV, Choose between template 'Friggeri' or 'Europass'")
+parser.add_argument('cv', metavar='cv', type=str, help='file information JSON')
+parser.add_argument('--template', dest='template', help='template name, choose between Friggeri or Europass')
+parser.add_argument('--output', dest='outname', action='store', help='Output name file.')
 
 if __name__ == '__main__':
-    with open("information.json") as f:
-        data = json.load(f)
-
-    setup_curriculum_vitae("Friggeri", data, "FrancescoArgentieri-Resume.tex")
-    #setup_curriculum_vitae("europass", data, "FrancescoArgentieri-Resume-europass.tex")
+    args = parser.parse_args()
+    data_resume = Identifying()
+    data_resume.read_from_file(args.cv)
+    setup_curriculum_vitae(args.template, data_resume.data, args.outname)
     sys.exit()
